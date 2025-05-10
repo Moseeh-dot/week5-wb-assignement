76f62164d4b629e6521c818f45acb43b9afda93f
Moseeh-dot <moses123.munene@gmail.com>
2025-05-10 14:24:54 +0300

    Add files via upload

---
+++ me.js
@@ -0,0 +1,32 @@
+
+let name = input("Enter your name: ");
+const age = 25;
+  
+let greet = function() {
+    console.log("Hello, " + name);
+}
+let welcome = function() {
+    console.log("Welcome to the JavaScript world, " + name);
+}
+
+let farewell = function() {
+    console.log("Goodbye, " + name);
+}
+let askAge = function() {
+   
+    console.log("You are " + age + " years old.");
+}
+function greetUser(firtname,lastname) {
+    return "Hello, " + firtname + " " + lastname;
+}
+function welcomeUser(firstname, lastname) {
+    return "Welcome to the MunCop, " + firstname + " " + lastname;
+}
+
+
+
+
+
+
+
+
